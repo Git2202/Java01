28d14ea474514316665b67a63b88f8db04c116da
Isra Irshad <isra.23cs049@sode-edu.in>
2024-09-10 20:35:09 +0530

    Create Lab1.java

---
+++ Lab1.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+public class Lab1{
+  public static void main(String[] args){
+    int N = Integer.parseInt(args[0]);
+    int[][] matrix1 = new int[N][N];
+    int[][] matrix2 = new int[N][N];
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the elements for first array");
+    for(int i=0;i<N;i++)
+      {
+        for(int j=0;j<N;j++)
+          {
+            matrix1[i][j] = scanner.nexrInt();
+          }
+      }
+    System.out.println("Enter the elements for second array");
+    for(int i=0;i<N;i++)
+      {
+        for(int j=0;j<N;j++)
+          {
+            matrix2[i][j] = scanner.nexrInt();
+          }
+      }
+    int[][] sumMat = new int[N][N];
+    for(int i=0;i<N;i++)
+      {
+        for(int j=0;j<N;j++)
+          {
+           sumMat[i][j] = matrix1[i][j] + matrix2[i][j];
+          }
+      }
+    System.out.println("The first matrix is");
+    for(int i=0;i<N;i++)
+      {
+        for(int j=0;j<N;j++)
+          {
+           System.out.print(matrix1 + "\t");
+          }
+        System.out.println();
+      }
+    System.out.println("The second matrix is");
+    for(int i=0;i<N;i++)
+      {
+        for(int j=0;j<N;j++)
+          {
+           System.out.print(matrix2 + "\t");
+          }
+        System.out.println();
+      }
+    System.out.println("The sum of the matrix is");
+    for(int i=0;i<N;i++)
+      {
+        for(int j=0;j<N;j++)
+          {
+           System.out.print(sumMat + "\t");
+          }
+        System.out.println();
+      }
+  }
+}
