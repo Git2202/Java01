4fa64194692bac4772485b92438e586efc32250c
Isra Irshad <isra.23cs049@sode-edu.in>
2024-11-06 15:56:15 +0530

    StringOperations_And_OperationsOnFiles

---
+++ M7.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1;
+import pkg2.Tools2;
+public class M7 {
+    public static void main(String[] args){
+        Tools2 t1=new Tools2();
+        System.out.println(t1.reverse("Isra Irshad"));
+    }
+}
