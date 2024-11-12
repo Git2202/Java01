f04c00ea5e5cf1e0a0c495c95f60836ca31e0514
Isra Irshad <isra.23cs049@sode-edu.in>
2024-11-12 16:23:49 +0530

    Static_Keyword

---
+++ IS19.java
@@ -0,0 +1,12 @@
+public class IS19 {
+    static int count=0;
+    public static void increament(){
+        count++;
+    }
+    public static void main(String[] args){
+        IS19.increament();
+        IS19.increament();
+        System.out.println("Count:"+count);
+    }
+    
+}
